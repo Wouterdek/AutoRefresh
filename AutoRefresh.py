ad8397b642ad93f6f7717bc8ac20eded8384e3a4
Waterflames <wouterdekrsmckr@gmail.com>
2015-01-10 11:46:38 +0100

    Rate settings + check if refresh is already enabled

--- AutoRefresh.py
+++ AutoRefresh.py
@@ -6,25 +6,34 @@ refreshThread = None
 
 class EnableAutoRefreshCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		settings = sublime.load_settings('AutoRefresh.sublime-settings')
+		refreshRate = settings.get('auto_refresh_rate')
+		if refreshRate == None or (type(refreshRate) is not int and type(refreshRate) is not float):
+			print("Invalid auto_refresh_rate setting, using default 3")
+			refreshRate = 3
+
 		global refreshThread
-		refreshThread = RefreshThread(self, edit)
-		refreshThread.start()
+		if refreshThread == None or not refreshThread.enabled:
+			refreshThread = RefreshThread(self, edit, refreshRate)
+			refreshThread.start()
 
 class DisableAutoRefreshCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		global refreshThread
-		refreshThread.enabled = False
+		if refreshThread != None:
+			refreshThread.enabled = False
 
 class RefreshThread(threading.Thread):
-	def __init__(self, cmd, edit):
-		threading.Thread.__init__(self)
+	def __init__(self, cmd, edit, refreshRate):
 		self.cmd = cmd
 		self.edit = edit
 		self.enabled = True
+		self.refreshRate = refreshRate;
+		threading.Thread.__init__(self)
 
 	def run(self):
 		while self.enabled:
-			time.sleep(3)
+			time.sleep(self.refreshRate)
 			sublime.set_timeout(self.callback, 1)
 
 	def callback(self):
